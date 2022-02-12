0e72fc3a61c2a046b5aec207a2bdad7f4f07cd9d
Admin <akash.kotkar321@gmail.com>
2022-02-12 10:17:06 +0530

    added crossorigin on all ,java files

--- src/main/java/com/poc/controller/RegistrationController.java
+++ src/main/java/com/poc/controller/RegistrationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -46,7 +47,7 @@ public class RegistrationController {
 		
 	}
 	
-	@RequestMapping(value="/userupdate/{id}",method =RequestMethod.PUT, consumes ="application/json")
+	@PutMapping(value="/userupdate/{id}",consumes ="application/json")
 	@CrossOrigin
 	public ResponseEntity<Object> updateUser(@RequestBody User user,@PathVariable int id) {
 
